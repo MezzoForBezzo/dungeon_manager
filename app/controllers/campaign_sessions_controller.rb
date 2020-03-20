d82531c2352f3da9434286b1ec8a88db76d9f541
MezzoForBezzo <michael.ashdown.berry@gmail.com>
2020-03-20 14:12:39 +1300

    Added campaign sessions to campaigns

--- app/controllers/campaign_sessions_controller.rb
+++ app/controllers/campaign_sessions_controller.rb
@@ -1,18 +1,24 @@
 class CampaignSessionsController < ApplicationController
+  before_action :get_campaign
+  before_action :set_campaign_session, only: [:show, :edit, :update, :destroy]
+
   layout 'main'
   def index
-    @campaign_sessions = CampaignSession.all
+    @campaign_sessions = @campaign.campaign_sessions
+  end
+
+  def show
   end
 
   def new
-   @campaign_session = CampaignSession.new
+   @campaign_session = @campaign.campaign_sessions.build
   end
 
   def create
-    @campaign_session = CampaignSession.new(campaign_session_params)
+    @campaign_session = @campaign.campaign_sessions.build(campaign_session_params)
 
     if @campaign_session.save
-      redirect_to @campaign_session
+      redirect_to campaign_campaign_sessions_path(@campaign)
     else
       render 'new'
     end
@@ -23,7 +29,7 @@ class CampaignSessionsController < ApplicationController
 
   def update
     if @campaign_session.update(campaign_session_params)
-      redirect_to @campaign_session
+      redirect_to campaign_campaign_session_path(@campaign)
     else
       render 'edit'
     end
@@ -32,12 +38,20 @@ class CampaignSessionsController < ApplicationController
   def destroy
     @campaign_session.destroy
 
-    redirect_to campaigns_path
+    redirect_to campaign_campaign_session_path(@campaign)
   end
 
   private
 
+  def get_campaign
+    @campaign ||= Campaign.find(params[:campaign_id])
+  end
+
+  def set_campaign_session
+    @campaign_session ||= @campaign.campaign_sessions.find(params[:id])
+  end
+
   def campaign_session_params
-    params.require(:campaign_session).permit(:name, :description, :campaign)
+    params.require(:campaign_session).permit(:name, :description, :campaign_id, :notes)
   end
 end
